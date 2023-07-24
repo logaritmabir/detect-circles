d2aa7b7595d27456cc45d7142ca7fa1276cbb9bb
ben <emre.ozer@bil.omu.edu.tr>
2023-07-24 10:31:24 +0300

    latest

--- detect-circles/main.cpp
+++ detect-circles/main.cpp
@@ -21,23 +21,13 @@ int main(void) {
 	Mat frame;
 
 	Mat input = imread("C:\\Users\\steam\\OneDrive\\Masaüstü\\ball-images\\circles.png", IMREAD_COLOR);
-	Mat hsv_input;
-	Mat input_gray, hsv_input_gray;
-
+	Mat hsv_input, input_gray, hsv_input_gray;
 	Mat edge;
-
 	Mat mask1, mask2, input_masked, hsv_input_gray_masked;
-
 	Point origin(0, 0);
 
 	vector<double> distances;
 	vector<int>index_list;
-
-
-	Mat sharpening_kernel = (Mat_<double>(3, 3) << -1, -1, -1,
-		-1, 9, -1,
-		-1, -1, -1);
-	Mat sharpen, sharpen_gray, sharpen_edge;
 
 	while (1) {
 		capture.read(frame);
@@ -48,42 +38,43 @@ int main(void) {
 
 		GaussianBlur(input, input, Size(3, 3), 2, 2, BORDER_REFLECT_101);
 
-		cvtColor(input, hsv_input, COLOR_BGR2HSV); /*cvtColor parameters : input image, output image, conversion style*/
+		cvtColor(input, hsv_input, COLOR_BGR2HSV);
 		cvtColor(input, input_gray, COLOR_BGR2GRAY);
 		cvtColor(hsv_input, hsv_input_gray, COLOR_BGR2GRAY);
 
 		/*Masking for red*/
-
 		inRange(hsv_input, Scalar(0, 70, 50), Scalar(10, 255, 255), mask1);
 		inRange(hsv_input, Scalar(170, 70, 50), Scalar(180, 255, 255), mask2);
 		input_masked = (mask1 | mask2);
 
+		Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(9, 9));
+		morphologyEx(input_masked, input_masked, MORPH_OPEN, kernel);
+		morphologyEx(input_masked, input_masked, MORPH_CLOSE, kernel);
+
 		hsv_input_gray_masked = hsv_input_gray & input_masked;
 
-		Canny(input_gray, edge, 50, 150, 3);
+		//Canny(input_gray, edge, 50, 150, 3);
 
 		vector<Vec3f> circles; /*element of circle_centers -> (x,y,radius) */
 
-		int pieces = 11;
+		int pieces = 5;
 		int radius = pieces * 2;
-		int min_dist = input_gray.cols/(2 * radius);
+		int min_dist = input_gray.cols / pieces;
 		int min_radius = input_gray.cols / radius;
 		int max_radius = input_gray.cols / (radius - 2);
 
-		HoughCircles(hsv_input_gray_masked, circles, HOUGH_GRADIENT,1.5, min_dist,50,40, 0, max_radius);
-
-		/*gotta work on blob detectin and matchtemplate funcs then also otsu's threshold..*/
+		HoughCircles(hsv_input_gray_masked, circles, HOUGH_GRADIENT,1.5, min_dist,50,40, min_radius, max_radius);
 
 		for (int i = 0; i < circles.size(); i++) {
-			Point center(cvRound(circles[i][0]), cvRound(circles[i][1])); /*HoughCircle returns the centers of the circles in floating number.*/
+			Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
 			int radius = cvRound(circles[i][2]);
 			circle(input, center, 3, Scalar(0, 255, 0), -1, 8, 0);
-			circle(input, center, radius, Scalar(255, 0, 0), 3, 8, 0); /*BGR*/
+			circle(input, center, radius, Scalar(255, 0, 0), 3, 8, 0); 
 			double dist = sqrt(pow(origin.x - center.x, 2) + pow(origin.y - center.y, 2));
 			distances.push_back(dist);
 		}
 
-		for (int i = 0; i < distances.size(); i++) { /*Generating index list for memory optimization high ratio access speed*/
+		for (int i = 0; i < distances.size(); i++) {
 			int nearest = find_nearest_circle(distances);
 			index_list.push_back(nearest);
 		}
@@ -92,7 +83,7 @@ int main(void) {
 			Point center = Point(cvRound(circles[index_list.at(i)][0]), cvRound(circles[index_list.at(i)][1]));
 			putText(input, to_string(i), center, FONT_HERSHEY_PLAIN, 1.5, Scalar(255, 200, 100), 2);
 		}
-		imshow("trackEdge", edge);
+		//imshow("trackEdge", edge);
 		imshow("trackHough", input);
 		imshow("Input HSV Masked Red Channel", input_masked);
 		waitKey(1);
@@ -116,9 +107,3 @@ int find_nearest_circle(vector<double>& distances) {
 //	distances.at(max_index) = -1;
 //	return max_index;
 //}
-
-/*Median filter could be used instead of gaussian filter for reducing the noise in the input image. They should be compared for utilization at the end*/
-/*When will be applied the image enhancement ? After conversion to GRAY or before ? */
-/*Imperfect : Kusurlu , robust : güçlü,dayanýklý, aperture : açýklýk*/
-/*HoughCircles() function by openCV : Usually that detects the center of the circle. It could be used to measure distance*/
-/*circle() funciton by openCV : Generates a circle which has Point in input image with spesific radius value*/
