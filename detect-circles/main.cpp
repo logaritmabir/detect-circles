c2afae3f737084e8425dab8870f19f918a745ed0
ben <emre.ozer@bil.omu.edu.tr>
2023-07-26 16:13:34 +0300

    gonna imp CLAHE

--- detect-circles/main.cpp
+++ detect-circles/main.cpp
@@ -24,7 +24,8 @@ int main(void) {
 		return EXIT_FAILURE;
 	}
 
-	Mat input = imread("C:\\Users\\steam\\OneDrive\\Masaüstü\\ball-images\\circles.png", IMREAD_COLOR);
+	Mat input;
+	//input = imread("C:\\Users\\steam\\OneDrive\\Masaüstü\\ball-images\\circles.png", IMREAD_COLOR);
 	Point origin(0, 0);
 
 	while (1) {
@@ -32,44 +33,45 @@ int main(void) {
 		capture.read(frame);
 		input = frame;
 
-		GaussianBlur(input, input, Size(3, 3), 2, 2, BORDER_REFLECT_101);/*clear noise*/
+		GaussianBlur(input, input, Size(3, 3), 2, 2, BORDER_REFLECT_101);
 
+		Mat gamma_cor_input;
 		float gamma = 0.8f;
 		Mat lookUpTable(1, 256, CV_8U);
 		uchar* p = lookUpTable.ptr();
 		for (int i = 0; i < 256; ++i)
 			p[i] = saturate_cast<uchar>(pow(i / 255.0, gamma) * 255.0);
-		LUT(input, lookUpTable, input);
+		LUT(input, lookUpTable, gamma_cor_input);
 
+		Mat equalized_input;
 		vector<Mat> channels;
 		split(input, channels);
 
 		for (int i = 0; i < channels.size(); i++) {
 			equalizeHist(channels[i], channels[i]);
 		}
-		merge(channels, input);
+		merge(channels, equalized_input);
 
 		Mat hsv_input, hsv_input_gray;
-		cvtColor(input, hsv_input, COLOR_BGR2HSV); /*conver input to hsv*/
-		cvtColor(hsv_input, hsv_input_gray, COLOR_BGR2GRAY);/*gray hsv for HoughCircle()*/
+		cvtColor(input, hsv_input, COLOR_BGR2HSV);
+		cvtColor(hsv_input, hsv_input_gray, COLOR_BGR2GRAY);
 
-		/*Masking for red*/
 		Mat mask1, mask2;
-		inRange(hsv_input, Scalar(0, 70, 50), Scalar(10, 255, 255), mask1);/*red channel masking*/
+		inRange(hsv_input, Scalar(0, 70, 50), Scalar(10, 255, 255), mask1);
 		inRange(hsv_input, Scalar(170, 70, 50), Scalar(180, 255, 255), mask2);
-		Mat mask = (mask1 | mask2);/*combining masks*/
+		Mat mask = (mask1 | mask2);
 
-		Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(9, 9)); /*morphological enhancement*/
+		Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(9, 9));
 		morphologyEx(mask, mask, MORPH_OPEN, kernel);
 		morphologyEx(mask, mask, MORPH_CLOSE, kernel);
 
-		Mat hsv_input_gray_masked = hsv_input_gray & mask; /*masking*/
+		Mat hsv_input_gray_masked = hsv_input_gray & mask;
 
 		Mat mask3d, hsv_input_masked;
 		cvtColor(mask, mask3d, COLOR_GRAY2BGR);
-		hsv_input_masked = mask3d & hsv_input; /*masking colorful image masking*/
+		hsv_input_masked = mask3d & hsv_input;
 
-		vector<Vec3f> circles; /*element of circle_centers -> (x,y,radius) */
+		vector<Vec3f> circles;
 
 		int pieces = 5;
 		int radius = pieces * 2;
@@ -84,16 +86,16 @@ int main(void) {
 		for (int i = 0; i < circles.size(); i++) {
 			Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
 			int radius = cvRound(circles[i][2]);
-			circle(input, center, 3, Scalar(0, 255, 0), -1, 8, 0);/*center of circle*/
-			circle(input, center, radius, Scalar(255, 0, 0), 3, 8, 0);/*outside of circle*/
-			circle(roi, center, radius, Scalar(255, 255, 255), -1, 8, 0);/*masking the circle*/
+			circle(input, center, 3, Scalar(0, 255, 0), -1, 8, 0);
+			circle(input, center, radius, Scalar(255, 0, 0), 3, 8, 0);
+			circle(roi, center, radius, Scalar(255, 255, 255), -1, 8, 0);
 			double dist = sqrt(pow(origin.x - center.x, 2) + pow(origin.y - center.y, 2));
 			distances.push_back(dist);
 		}
 
 		Mat roi3d;
 		cvtColor(roi, roi3d, COLOR_GRAY2BGR);
-		Mat pixels = hsv_input_masked & roi3d; /*görüntünün dairesel kısmının renkli hsv olarak alınması*/
+		Mat pixels = hsv_input_masked & roi3d;
 
 		vector<int>index_list;
 		for (int i = 0; i < distances.size(); i++) {
@@ -106,13 +108,10 @@ int main(void) {
 			putText(input, to_string(i), center, FONT_HERSHEY_PLAIN, 1.5, Scalar(255, 200, 100), 2);
 		}
 
-		Mat bla;
-		cvtColor(pixels, bla, COLOR_HSV2BGR);
-		imshow("bla", bla);
-		imshow("roi", roi);
-		imshow("pixels", pixels);
 		imshow("input", input);
-		imshow("Masked HSV Input", hsv_input_masked);
+		imshow("equalized_input", equalized_input);
+		imshow("gamma_cor_input", gamma_cor_input);
+
 		waitKey(1);
 
 		distances.clear();
@@ -125,15 +124,6 @@ int find_nearest_circle(vector<double>& distances) {
 		if (distances.at(i) < distances.at(min))
 			min = i;
 	}
-	distances.at(min) = INT_MAX; /*hmm*/
+	distances.at(min) = INT_MAX;
 	return min;
 }
-//int find_farthest_circle(vector<double>& distances) {
-//	int max_index = 0;
-//	for (int i = 0; i < distances.size(); i++) {
-//		if (distances.at(i) > distances.at(max_index))
-//			max_index = i;
-//	}
-//	distances.at(max_index) = -1;
-//	return max_index;
-//}
